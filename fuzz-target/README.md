cf4415c05737e7a3508d018e520398d78ef64603
dbr <dbr.onix@gmail.com>
2023-10-17 12:21:49 +1030

    Update fuzzing lock file. Also fuzz in debug mode as more likely to panic via debug assertions

---
+++ fuzz-target/README.md
@@ -0,0 +1,3 @@
+To run fuzzing, install the [cargo-afl](https://crates.io/crates/afl) tool with `cargo install cargo-afl`
+
+Then run the `./run-fuzzing.sh` script
--- fuzz-target/run-fuzzer.sh
+++ fuzz-target/run-fuzzer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo afl build --release && env AFL_SKIP_CPUFREQ=1 cargo afl fuzz -i ./in -o ./out ./target/release/fuzz-target
+cargo afl build && env AFL_SKIP_CPUFREQ=1 cargo afl fuzz -i ./in -o ./out ./target/debug/fuzz-target
